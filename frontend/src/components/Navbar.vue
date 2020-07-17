40efeb89b2270e36fea58f598db610af05e8a6f7
Romain Albasini <->
2020-07-17 13:53:54 +0200

    Fix Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  b-navbar.border-bottom(toggleable="lg" type="light" variant="light")
+    b-button(variant="primary" @click="toggleSidebar")
+      b-icon(icon="list" aria-hidden="true")
+        span(class="sr-only") Help
+    //- b-navbar-brand Blabla
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "Navbar",
+  methods: mapActions(["toggleSidebar"])
+};
+</script>
+
+<style>
+</style>
