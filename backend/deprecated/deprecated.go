ddaf5c4cac0d529e8d3453462e55e4b03a8ee1ad
Romain Albasini <->
2020-07-17 14:07:53 +0200

    Fix Navbar.vue (1/2)

--- backend/deprecated/deprecated.go
+++ backend/deprecated/deprecated.go
@@ -5,7 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"github.com/wayoos/kubedash/backend/kublink"
-	_ "github.com/wayoos/kubedash/backend/tools"
+	"github.com/wayoos/kubedash/backend/tools"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -24,6 +24,8 @@ func deprecated(c echo.Context) error {
 
 	var ns *v1.NamespaceList
 	ns, _ = kublink.ClientSet().CoreV1().Namespaces().List(metav1.ListOptions{})
+
+	tools.AnalyzeDeprecatedResources(ns)
 
 	return c.JSON(http.StatusOK, ns.Items)
 }
